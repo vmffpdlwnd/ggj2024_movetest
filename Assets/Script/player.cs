395ec8364bd16ddf788292fd2d3b56319de3df74
vmffpdlwnd <leegunehee@gmail.com>
2024-01-26 20:42:10 +0900

    점프 추가
    점프 추가, 속도 조절

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -6,16 +6,37 @@ public class player : MonoBehaviour
 {
     Rigidbody2D rigid;
 
+    [SerializeField]
+    private float jumpPower;
+    [SerializeField]
+    private float speed;
+
+    float jumpCount = 0;
+
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-
     }
 
     private void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.right * h * speed, ForceMode2D.Impulse);
     }
-    
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount == 0)
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jumpCount++;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0;
+        }
+    }
 }
